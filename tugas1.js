40ef12a4754f36e48a3cbdf6d54ef112e3f383ad
sitimarni <124442828+sitimarni@users.noreply.github.com>
2023-09-29 18:21:25 +0700

    Add files via upload

---
+++ tugas1.js
@@ -0,0 +1,15 @@
+let identitas = 'identitas'
+console.log('identitas')
+var nama = 'siti marni'
+let nim = '20220040029'
+console.log('nama saya = ' + nama )
+console.log('nim saya = ' + nim )
+// aritmatika
+let aritmatika = 'aritmatika'
+console.log(aritmatika)
+let a = 30;
+let b = 6;
+console.log(a + ' * ' + b + ' = ' +a*b)
+console.log(a + ' + ' + b + ' = ' +(a+b))
+console.log(a + ' / ' + b + ' = ' +a/b)
+console.log(a + ' - ' + b + ' = ' + (a-b))
